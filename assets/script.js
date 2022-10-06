e62ac91a58660abce33b124fcd1ca5114090dc88
Felix Prawira <felix@stendard.io>
2022-10-06 21:09:24 +0700

    Update: reusable debounce

--- assets/script.js
+++ assets/script.js
@@ -5,34 +5,19 @@ var win = false
 var start = false
 var clicks = 0
 var time = 0
-var timeout;
+
 const mole_img_url = new URL("mole.60ea17ce.png", import.meta.url).href
+var win_msg = document.querySelector('.win-message')
+var start_btn = document.querySelector('.start-button')
+var txt_time = document.querySelector('.time')
+var txt_clicks = document.querySelector('.clicks')
 
 var set_mole_location = () =>  Math.floor(Math.random() * holes.length)
 document.querySelector('.start-button').addEventListener('click', start_game)
 
-function clickHole(event) {
-    clicks++
-    document.querySelector('.clicks').textContent = `Clicks: ${clicks}`
-    hit(event)
-    debounce()
-}
-
 function setClickListener() {
     for (let i = 0; i < holes.length; i++) {
         holes[i].addEventListener('click', clickHole)
-    }
-}
-
-function hit(event) {
-    if (event.target.tagName == 'IMG')
-    {
-        win = true
-        document.querySelector('.start-button').removeAttribute('disabled')
-        document.querySelector('.win-message').textContent = 'You win!'
-        setTimeout(() => {
-            alert(`Gotcha! time to catch ${time} seconds and ${clicks} clicks`)
-        }, 100)        
     }
 }
 
@@ -41,41 +26,69 @@ setClickListener()
 const debounceAction = (l, e) => {
     if (!win)
     {
-        clearTimeout(timeout)
-        timeout = setTimeout(() => l.apply(), e)        
+        let timeout;
+
+        return (...args) => {
+            clearTimeout(timeout)
+            timeout = setTimeout(() => l.apply(null, args), e)
+        }        
     }
 }, randomInterval = (l, e) => Math.floor(l + Math.random() * (e - l + 1));
 
-var show_mole = () => {
-    var mole_img = document.createElement('img')
-    mole_img.setAttribute('src', mole_img_url)
-    mole_img.classList.add('mole')
+const debounce = debounceAction(() => show_mole(), 1e3)
 
-    holes[set_mole_location()].children[0].append(mole_img), setTimeout(() => {
-                hide_mole()
-            }, randomInterval(200, 400))
-}
-
-var hide_mole = () => {
+function clickHole(event) {
     if (!win)
     {
-        for(var i = 0; i < holes.length; i++) {
-            if (holes[i].children[0].hasChildNodes()) {
-                holes[i].children[0].removeChild(holes[i].children[0].firstChild)
-            }
-        }
-        debounce()        
+        clicks++
+        txt_clicks.textContent = `Clicks: ${clicks}`
+        hit(event), debounce()        
+    }
+    else {
+        alert("You already catch the mole, restart the game!")
     }
 }
 
-var debounce = () => debounceAction(show_mole, 1e3)
+function hit(event) {
+    if (event.target.tagName == 'IMG')
+    {
+        win = true
+        start_btn.removeAttribute('disabled')
+        win_msg.textContent = 'You win!'
+        setTimeout(() => {
+            alert(`Gotcha! time to catch ${time} seconds and ${clicks} clicks`)
+        }, 100)
+    }
+}
+
+function show_mole() {
+    if (!win)
+    {
+        var mole_img = document.createElement('img')
+        mole_img.setAttribute('src', mole_img_url)
+        mole_img.classList.add('mole')
+
+        holes[set_mole_location()].children[0].append(mole_img), setTimeout(() => {
+                    win || hide_mole()
+                }, randomInterval(200, 400))        
+    }
+}
+
+var hide_mole = () => {
+    for(var i = 0; i < holes.length; i++) {
+        if (holes[i].children[0].hasChildNodes()) {
+            holes[i].children[0].removeChild(holes[i].children[0].firstChild)
+        }
+    }
+    debounce()
+}
 
 function timer() {
     setInterval(() => {
         if (!win)
         {
             time += 1
-            document.querySelector('.time').textContent = `Elapsed time: ${time} seconds`        
+            txt_time.textContent = `Elapsed time: ${time} seconds`        
         }
     }, 1000)
 }
@@ -84,9 +97,9 @@ function start_game() {
     start = true
     win = false
     timer()
-    document.querySelector('.start-button').setAttribute('disabled', 1)
-    document.querySelector('.time').textContent = `Elapsed time: 0 seconds`
-    document.querySelector('.clicks').textContent = `Clicks: 0`
-    document.querySelector('.win-message').textContent = ''
+    start_btn.setAttribute('disabled', 1)
+    txt_time.textContent = `Elapsed time: 0 seconds`
+    txt_clicks.textContent = `Clicks: 0`
+    win_msg.textContent = ''
     show_mole()
 }
